1004d5757d6979fcfb30518794fc03898ee0f3a2
Екатерина <59869569+aristovaE@users.noreply.github.com>
2020-10-03 14:33:09 +0300

    открытие окна декларанта через ShowWindow

---
+++ Test2/Test2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Process.Start();
+            IntPtr hwnd = FindWindow(null, "ВЭД-Декларант");
+            //Console.WriteLine("hwnd: " + hwnd);
+            if (IsIconic(hwnd))
+            {
+                ShowWindow(hwnd, 9); //9 - restore
+            }
+            else
+            {
+                SetForegroundWindow(hwnd);
+
+            }
+            Console.WriteLine("вааау а ты и правда садистка");
+            Console.ReadKey();
+
+        }
+
+        #region native FindWindow, IsIconic, SetForegroundWindow, ShowWindow
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(String lpClassName, String windowName);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool IsIconic(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd, int showWindowCommand);
+        #endregion
+
+    }
+}
