994b6bec70432062d9e381408be140a5137554a5
Екатерина <59869569+aristovaE@users.noreply.github.com>
2020-10-05 17:48:49 +0300

    Попытка обращения к меню через winAPI - не работает

--- Test2 — копия/Test2/Program.cs
+++ Test2 — копия/Test2/Program.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿#define WM_COMMAND                      0x0111;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Messaging;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 using System.Windows.Automation;
+using System.Windows.Forms;
+using static System.Windows.Forms.Control;
 
 namespace Test2
 {
@@ -14,7 +18,6 @@ namespace Test2
     {
         static void Main(string[] args)
         {
-
             IntPtr hwnd = FindWindow(null, "ВЭД-Декларант");
             //"ВЭД-Декларант (незарегистрированная версия) 9.97 от 01.10.2020 (10000000/220719/0000005) - ТОЛЬКО ЧТЕНИЕ - [ДТ (основной лист)]"
             
@@ -25,26 +28,26 @@ namespace Test2
             else
             {
                 SetForegroundWindow(hwnd);
-
             }
-            Thread.Sleep(1000);
-           
 
-            var process = Process.GetProcessesByName("DCL").FirstOrDefault();
-           // IntPtr handle = process.MainWindowHandle;
-            IntPtr HMENU = GetMenu(process.MainWindowHandle); //не работает
-            //PostMessage(handle, WM_COMMAND, 2, 0); // File->New subtitle
-            var window = AutomationElement.FromHandle(process.MainWindowHandle);
-           // var window = AutomationElement.GetWindowByName("ВЭД-Декларант");
-            //Thread.Sleep(5000);
-            var menuBar = window.FirstChildByType(ControlType.MenuBar);
-            var fileMenu = menuBar.FirstDescendantByTypeAndName(ControlType.MenuItem, "Документ");
-            // раскрыли меню 
-            fileMenu.GetPattern<ExpandCollapsePattern>().Expand();
-            Thread.Sleep(100);
+            //var process = Process.GetProcessesByName("DCL").FirstOrDefault();
+            Thread.Sleep(5000);
+            // Получаем hwnd окна
+            IntPtr hWnd = WindowFromPoint(System.Windows.Forms.Cursor.Position);
+
+            //Определяем текст
+            StringBuilder builder = new StringBuilder(100);
+            GetWindowText(hWnd, builder, builder.Capacity);
+            string text = builder.ToString();
+
+            //Определяем класс
+            StringBuilder buffer = new StringBuilder(256);
+            GetClassName(hWnd, buffer, buffer.Capacity);
+            string className = buffer.ToString();
+            //  PostMessage(hwnd, WM_COMMAND, 2, 0); // WM_COMMAND??? https://stackoverflow.com/questions/9397700/c-sharp-winapi-clicking-on-menu-items
 
         }
-        
+
         #region native FindWindow, IsIconic, SetForegroundWindow, ShowWindow
         [DllImport("user32.dll", SetLastError = true)]
         static extern IntPtr FindWindow(String lpClassName, String windowName);
@@ -60,62 +63,23 @@ namespace Test2
         [DllImport("user32.dll")] public static extern IntPtr WindowFromPoint(Point pt);
 
         [DllImport("user32.dll")]
-        public static extern IntPtr PostMessage(IntPtr hWnd, Message msg, int wParam, int lParam);
+        public static extern IntPtr PostMessage(IntPtr hWnd, System.Messaging.Message msg, int wParam, int lParam);
+       
         [DllImport("user32.dll")]
         static extern IntPtr GetMenu(IntPtr hWnd);
 
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool ShowWindow(IntPtr hWnd, int showWindowCommand);
+
+        [DllImport("user32", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+        [DllImport("user32", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
         #endregion
 
     }
-    static class AutomationHelpers
-    {
-        static public T GetPattern<T>(this AutomationElement element)
-            where T : BasePattern
-        {
-            var pattern = (AutomationPattern)typeof(T).GetField("Pattern").GetValue(null);
-            return (T)element.GetCurrentPattern(pattern);
-        }
 
-        static public AutomationElement FirstChildByType(
-            this AutomationElement element, ControlType ct)
-        {
-            return element.FindFirst(
-                TreeScope.Children,
-                new PropertyCondition(AutomationElement.ControlTypeProperty, ct));
-        }
-
-        static public AutomationElement FirstDescendantByTypeAndName(
-            this AutomationElement element, ControlType ct, string name)
-        {
-            return element.FindFirst(
-                TreeScope.Descendants,
-                new AndCondition(
-                    new PropertyCondition(AutomationElement.ControlTypeProperty, ct),
-                    new PropertyCondition(AutomationElement.NameProperty, name)));
-           
-        }
-
-        static public AutomationElement FindWindowFrom(AutomationElement control)
-        {
-            var walker = TreeWalker.ControlViewWalker;
-            while (control.Current.ControlType != ControlType.Window)
-                control = walker.GetParent(control);
-            return control;
-        }
-        public static AutomationElement GetWindowByName(string name)
-        {
-            AutomationElement root = AutomationElement.RootElement;
-            foreach (AutomationElement window in root.FindAll(TreeScope.Children, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Window)))
-            {
-                if (window.Current.Name.Contains(name) && window.Current.IsKeyboardFocusable)
-                {
-                    return window;
-                }
-            }
-            return null;
-        }
-    }
 }
