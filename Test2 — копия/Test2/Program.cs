aee0cfe54cace719a62e92e72f6bc8e69d61e51d
Екатерина <59869569+aristovaE@users.noreply.github.com>
2020-10-06 17:56:15 +0300

    Оптимизация + комментарии

--- Test2 — копия/Test2/Program.cs
+++ Test2 — копия/Test2/Program.cs
@@ -30,33 +30,61 @@ namespace Test2
             }
 
             //var process = Process.GetProcessesByName("DCL").FirstOrDefault();
+            IntPtr windowDCL = WindowFromPoint(System.Windows.Forms.Cursor.Position);// окно области где сейчас находится курсор(либо меню, либо область работы с ДТ)
             Thread.Sleep(1000);
-            // Получаем hwnd окна
-            IntPtr hWnd = WindowFromPoint(System.Windows.Forms.Cursor.Position);// окно рабочей области БЕЗ меню
 
-            //симуляция передвижения мыши 
-
-            //MoveMouse(hWnd, SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height); //не работает - нет меню в области
+            //работает при запуске из VS
+            MoveMouseAndClick(windowDCL, 47, 30); //Меню  - "Документ"
+            MoveMouseAndClick(windowDCL, 96, 76); //"Выбрать ДТ"
             
-            //Нажатие кнопок меню через сочетания клавиш
-            SendKeys.SendWait("%д");
-            Thread.Sleep(1000);
-            SendKeys.SendWait("в");
-            Thread.Sleep(1000);
-            SendKeys.SendWait("~");
-        }
+            //  UseShortcuts();
 
-        private static void MoveMouse(IntPtr hWnd,int screenWidth, int screenHeight)
+
+        }
+        /// <summary>
+        /// Нажатие кнопок меню через сочетания клавиш (заранее переключиться на русскую раскладку)
+        /// </summary>
+        private static void UseShortcuts()
+        {
+            SendKeys.SendWait("%д"); // Меню - Документ
+            Thread.Sleep(1000);
+            SendKeys.SendWait("в"); // Открыть ДТ
+            Thread.Sleep(1000);
+            SendKeys.SendWait("~"); // Enter
+            Thread.Sleep(2000);
+            SendKeys.SendWait("{ESC}"); //Выход
+        }
+        /// <summary>
+        /// симуляция передвижения мыши
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="screenWidth"></param>
+        /// <param name="screenHeight"></param>
+        private static void MoveMouseAndClick(IntPtr hWnd,int pX, int pY)
         {
             Point p = new Point();
-            
-                p.X = Convert.ToInt16(17);
-                p.Y = Convert.ToInt16(23); //координаты из inspect exe
-                ClientToScreen(hWnd, ref p);
-                SetCursorPos(p.X, p.Y);
-                Thread.Sleep(1000);
+            p.X = Convert.ToInt16(pX);  //координаты из inspect exe
+            p.Y = Convert.ToInt16(pY);  //поле How found : Mouse Move(47,30) при наведении мышкой на нужное поле
+            ClientToScreen(hWnd, ref p);
+            SetCursorPos(p.X, p.Y);
+            DoMouseLeftClick(p.X, p.Y);
+            Thread.Sleep(1000);
         }
-
+        private static void MoveMouse(IntPtr hWnd)
+        {
+            Point p = new Point();
+            p.X = Convert.ToInt16(0);  //координаты из inspect exe
+            p.Y = Convert.ToInt16(0);  //поле How found : Mouse Move(47,30) при наведении мышкой на нужное поле
+            ClientToScreen(hWnd, ref p);
+            SetCursorPos(p.X, p.Y);
+            Thread.Sleep(1000);
+        }
+        private static void DoMouseLeftClick(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+        }
+        
         #region native FindWindow, IsIconic, SetForegroundWindow, ShowWindow
         [DllImport("user32.dll", SetLastError = true)]
         static extern IntPtr FindWindow(String lpClassName, String windowName);
@@ -69,6 +97,10 @@ namespace Test2
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool SetForegroundWindow(IntPtr hWnd);
 
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd, int showWindowCommand);
+
         [DllImport("user32.dll")] 
         public static extern IntPtr WindowFromPoint(Point pt);
 
@@ -78,16 +110,23 @@ namespace Test2
         [DllImport("user32.dll")]
         public static extern bool ClientToScreen(IntPtr hWnd, ref Point point);
 
-       
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dsFlags, int dx, int dy, int cButtins, int dsExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        //[DllImport("user32.dll")]
+        //static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
         // [DllImport("user32.dll")]
         // public static extern IntPtr PostMessage(IntPtr hWnd, System.Messaging.Message msg, int wParam, int lParam);
 
         [DllImport("user32.dll")]
         static extern IntPtr GetMenu(IntPtr hWnd);
 
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool ShowWindow(IntPtr hWnd, int showWindowCommand);
+        public const UInt32 MF_BYPOSITION = 0x00000400;
+
 
         //[DllImport("user32", SetLastError = true, CharSet = CharSet.Auto)]
         //public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
@@ -95,6 +134,14 @@ namespace Test2
         //[DllImport("user32", CharSet = CharSet.Auto, SetLastError = true)]
         //public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
 
+        [DllImport("user32.dll")]
+        static extern int GetMenuString(IntPtr hMenu, uint uIDItem, [Out] StringBuilder lpString, int nMaxCount, uint uFlag);
+        internal const UInt32 MF_BYCOMMAND = 0x00000000;
+
+        [DllImport("user32.dll")]
+        static extern int GetMenuItemCount(IntPtr hMenu);
+        [DllImport("user32.dll")]
+        static extern bool IsMenu(IntPtr hMenu);
         #endregion
 
     }
