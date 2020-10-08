c8d9fb404785a3b3ea9a3e72d5ec5f97658803c6
Екатерина <59869569+aristovaE@users.noreply.github.com>
2020-10-08 12:03:15 +0300

    Создание проекта с кнопками и функционалом, аналогичным проекту Test2

---
+++ DclTestForm/DclTestForm/DclTest.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace DclTestForm
+{
+    partial class DclTest
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openDT_btn = new System.Windows.Forms.Button();
+            this.enterKey_btn = new System.Windows.Forms.Button();
+            this.doAll_btn = new System.Windows.Forms.Button();
+            this.openDT_lbl = new System.Windows.Forms.Label();
+            this.enterKey_lbl = new System.Windows.Forms.Label();
+            this.doAll_lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openDT_btn
+            // 
+            this.openDT_btn.Location = new System.Drawing.Point(112, 63);
+            this.openDT_btn.Name = "openDT_btn";
+            this.openDT_btn.Size = new System.Drawing.Size(90, 30);
+            this.openDT_btn.TabIndex = 0;
+            this.openDT_btn.Text = "Выполнить";
+            this.openDT_btn.UseVisualStyleBackColor = true;
+            this.openDT_btn.Click += new System.EventHandler(this.openDT_btn_Click);
+            // 
+            // enterKey_btn
+            // 
+            this.enterKey_btn.Location = new System.Drawing.Point(112, 99);
+            this.enterKey_btn.Name = "enterKey_btn";
+            this.enterKey_btn.Size = new System.Drawing.Size(90, 30);
+            this.enterKey_btn.TabIndex = 1;
+            this.enterKey_btn.Text = "Выполнить";
+            this.enterKey_btn.UseVisualStyleBackColor = true;
+            // 
+            // doAll_btn
+            // 
+            this.doAll_btn.Location = new System.Drawing.Point(112, 135);
+            this.doAll_btn.Name = "doAll_btn";
+            this.doAll_btn.Size = new System.Drawing.Size(90, 30);
+            this.doAll_btn.TabIndex = 2;
+            this.doAll_btn.Text = "Выполнить";
+            this.doAll_btn.UseVisualStyleBackColor = true;
+            // 
+            // openDT_lbl
+            // 
+            this.openDT_lbl.AutoSize = true;
+            this.openDT_lbl.Location = new System.Drawing.Point(13, 72);
+            this.openDT_lbl.Name = "openDT_lbl";
+            this.openDT_lbl.Size = new System.Drawing.Size(70, 13);
+            this.openDT_lbl.TabIndex = 3;
+            this.openDT_lbl.Text = "Открыть ДТ";
+            // 
+            // enterKey_lbl
+            // 
+            this.enterKey_lbl.AutoSize = true;
+            this.enterKey_lbl.Location = new System.Drawing.Point(13, 108);
+            this.enterKey_lbl.Name = "enterKey_lbl";
+            this.enterKey_lbl.Size = new System.Drawing.Size(74, 13);
+            this.enterKey_lbl.TabIndex = 4;
+            this.enterKey_lbl.Text = "Ввести \"333\"";
+            // 
+            // doAll_lbl
+            // 
+            this.doAll_lbl.AutoSize = true;
+            this.doAll_lbl.Location = new System.Drawing.Point(13, 144);
+            this.doAll_lbl.Name = "doAll_lbl";
+            this.doAll_lbl.Size = new System.Drawing.Size(84, 13);
+            this.doAll_lbl.TabIndex = 5;
+            this.doAll_lbl.Text = "Выполнить всё";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 183);
+            this.Controls.Add(this.doAll_lbl);
+            this.Controls.Add(this.enterKey_lbl);
+            this.Controls.Add(this.openDT_lbl);
+            this.Controls.Add(this.doAll_btn);
+            this.Controls.Add(this.enterKey_btn);
+            this.Controls.Add(this.openDT_btn);
+            this.Name = "Form1";
+            this.Text = "DclTest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openDT_btn;
+        private System.Windows.Forms.Button enterKey_btn;
+        private System.Windows.Forms.Button doAll_btn;
+        private System.Windows.Forms.Label openDT_lbl;
+        private System.Windows.Forms.Label enterKey_lbl;
+        private System.Windows.Forms.Label doAll_lbl;
+    }
+}
+
---
+++ DclTestForm/DclTestForm/DclTest.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DclTestForm
+{
+    public partial class DclTest : Form
+    {
+        public DclTest()
+        {
+            InitializeComponent();
+        }
+
+        private void openDT_btn_Click(object sender, EventArgs e)
+        {
+            OpenDCL();
+            SendMessageAndClick(47, 30);    //наведение мыши через SendMessage
+            SendMessageAndClick(96, 76);
+            Thread.Sleep(1000);
+            SendMessageAndClick(1700, 930);
+            //MoveMouseAndClick(47, 30);    //наведение мыши через mouse_event
+            //MoveMouseAndClick(96, 76);
+            //EnterShortcuts();             //ввод комбинаций клавиш для открытия меню
+        }
+
+        /// <summary>
+        /// Открытие меню - Документ (sendMessage)
+        /// </summary>
+        private static void OpenDCL()
+        {
+            IntPtr hwnd = FindWindow(null, "ВЭД-Декларант");
+            //"ВЭД-Декларант (расширенная версия) 9.97 от 01.10.2020 (10000000/220719/0000004) - [ДТ (основной лист)]"
+            if (IsIconic(hwnd))
+            {
+                ShowWindow(hwnd, 9); //9 - restore
+            }
+            else
+            {
+                SetForegroundWindow(hwnd);
+            }
+        }
+
+        /// <summary>
+        /// Открытие меню - Документ (sendMessage)
+        /// </summary>
+        private static void SendMessageAndClick(int pX, int pY)
+        {
+            IntPtr windowDCLMenu = WindowFromPoint(System.Windows.Forms.Cursor.Position = new Point(pX, pY)); //Меню - Документ
+            SendMessage(windowDCLMenu, WM_MOUSEMOVE, (IntPtr)0, MakeParam(pX, pY));
+            DoMouseLeftClick(pX, pY);
+            Thread.Sleep(1000);
+        }
+
+        /// <summary>
+        /// Ввод цифр в поля ДТ
+        /// </summary>
+        private static void EnterKey()
+        {
+            //ввод в поле, где находится фокус с открытой ДТ (ИНН)
+            keybd_event(VK_3, 0, 0, 0); //цифра 3
+            keybd_event(VK_3, 0, KEYEVENTF_KEYUP, 0);
+            Thread.Sleep(200);
+            keybd_event(VK_3, 0, 0, 0); //цифра 3
+            keybd_event(VK_3, 0, KEYEVENTF_KEYUP, 0);
+            Thread.Sleep(200);
+            keybd_event(VK_3, 0, 0, 0); //цифра 3
+            keybd_event(VK_3, 0, KEYEVENTF_KEYUP, 0);
+        }
+        /// <summary>
+        /// Открытие меню - Документ (сочетание клавиш (keybd_event))
+        /// </summary>
+        private static void EnterShortcuts()
+        {
+            //Ввод комбинации Alt+д для открытия меню - Документ
+            keybd_event(VK_ALT, 0, 0, 0); //клавиша Alt
+            keybd_event(VK_L, 0, 0, 0); //буква Д
+            keybd_event(VK_ALT, 0, KEYEVENTF_KEYUP, 0);
+            keybd_event(VK_L, 0, KEYEVENTF_KEYUP, 0);
+            Thread.Sleep(1000);
+            keybd_event(VK_D, 0, 0, 0); //буква В
+            keybd_event(VK_D, 0, KEYEVENTF_KEYUP, 0);
+            Thread.Sleep(1000);
+            keybd_event(VK_RETURN, 0, 0, 0); // Enter
+            keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
+        }
+
+        ///// <summary>
+        ///// Нажатие кнопок меню через сочетания клавиш (заранее переключиться на русскую раскладку)
+        ///// </summary>
+        //private static void UseShortcuts()
+        //{
+        //    SendKeys.SendWait("%д"); // Меню - Документ
+        //    Thread.Sleep(1000);
+        //    SendKeys.SendWait("в"); // Открыть ДТ
+        //    Thread.Sleep(1000);
+        //    SendKeys.SendWait("~"); // Enter
+        //    Thread.Sleep(2000);
+        //    SendKeys.SendWait("{ESC}"); //Выход
+        //}
+
+        /// <summary>
+        /// симуляция передвижения мыши
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="screenWidth"></param>
+        /// <param name="screenHeight"></param>
+        private static void MoveMouseAndClick(int pX, int pY)
+        {
+            IntPtr windowDCLMenu = WindowFromPoint(System.Windows.Forms.Cursor.Position = new Point(pX, pY));
+            Point p = new Point();
+            p.X = Convert.ToInt16(pX);  //координаты из inspect exe
+            p.Y = Convert.ToInt16(pY);  //поле How found : Mouse Move(47,30) при наведении мышкой на нужное поле
+            //ClientToScreen(windowDCLMenu, ref p);
+            SetCursorPos(p.X, p.Y);
+            Thread.Sleep(1000);
+            DoMouseLeftClick(p.X, p.Y);
+            Thread.Sleep(1000);
+        }
+        /// <summary>
+        /// Нажатие ЛКМ
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        private static void DoMouseLeftClick(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+        }
+
+
+        /// <summary>
+        /// "помещение разных значений в старшие и в младшие биты"
+        /// </summary>
+        /// <param name="low"></param>
+        /// <param name="hight"></param>
+        /// <returns></returns>
+        public static IntPtr MakeParam(int low, int hight)
+        {
+            return (IntPtr)((low & 0xFFFF) | (hight << 16));
+        }
+
+        #region native FindWindow, IsIconic, SetForegroundWindow, ShowWindow
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(String lpClassName, String windowName);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool IsIconic(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd, int showWindowCommand);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr WindowFromPoint(Point pt);
+
+        [DllImport("user32.dll")]
+        public static extern long SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        public static extern bool ClientToScreen(IntPtr hWnd, ref Point point);
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dsFlags, int dx, int dy, int cButtins, int dsExtraInfo);
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, UInt32 dwFlags, int dwExtraInfo);
+
+        // [DllImport("user32.dll")]
+        // public static extern IntPtr PostMessage(IntPtr hWnd, System.Messaging.Message msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetMenu(IntPtr hWnd);
+
+        //[DllImport("user32", SetLastError = true, CharSet = CharSet.Auto)]
+        //public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+        //[DllImport("user32", CharSet = CharSet.Auto, SetLastError = true)]
+        //public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        static extern int GetMenuString(IntPtr hMenu, uint uIDItem, [Out] StringBuilder lpString, int nMaxCount, uint uFlag);
+
+
+        [DllImport("user32.dll")]
+        static extern int GetMenuItemCount(IntPtr hMenu);
+        [DllImport("user32.dll")]
+        static extern bool IsMenu(IntPtr hMenu);
+        [DllImport("User32.dll")]
+        public static extern Int32 SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+
+        internal const UInt32 MF_BYCOMMAND = 0x00000000;
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        public const int WM_MOUSEMOVE = 0x0200;
+        public const int WM_LBUTTONDOWN = 0x0201;
+        public const int WM_LBUTTONUP = 0x0202;
+        const byte VK_3 = 0x33;
+        const byte VK_ALT = 0x12;
+        const byte VK_L = 0x4C;
+        const byte VK_D = 0x44;
+        const byte VK_RETURN = 0x0D;
+        public const UInt32 KEYEVENTF_EXTENDEDKEY = 1;
+        public const UInt32 KEYEVENTF_KEYUP = 2;
+        public const UInt32 MF_BYPOSITION = 0x00000400;
+        #endregion
+    }
+}
