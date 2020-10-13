a351965531f4d394a5b4ffa43cad228b0b9c3170
Екатерина <59869569+aristovaE@users.noreply.github.com>
2020-10-13 17:45:14 +0300

    Класс для хранения информации о полях

---
+++ DclTestForm/DclTestForm/TextBoxInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DclTestForm
+{
+    class TextBoxInfo
+    {
+        IntPtr handle;
+        string caption;
+        //rect? coords? type???
+
+        public TextBoxInfo(IntPtr handle, string caption)
+        {
+            this.handle = handle;
+            this.caption = caption;
+        }
+    }
+    
+}
